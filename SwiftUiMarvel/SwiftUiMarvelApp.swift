4ef17ea1073a6d19c70a881a0eed0644cdbb7a51
ch.laliotis <ch.laliotis@amco.gr>
2023-07-17 17:48:23 +0300

    Initial Commit

---
+++ SwiftUiMarvel/SwiftUiMarvelApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUiMarvelApp.swift
+//  SwiftUiMarvel
+//
+//  Created by Charilaos Laliotis on 17/7/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUiMarvelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
